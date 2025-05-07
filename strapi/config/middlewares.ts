25b75bc5308e424ec7763da6a6f5fae21f0e4d19
meetqy <meetqy@qq.com>
2025-05-07 20:44:54 +0800

    Refactor code structure for improved readability and maintainability

--- strapi/config/middlewares.ts
+++ strapi/config/middlewares.ts
@@ -1,12 +1,44 @@
-export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+export default ({ env }) => [
+  "strapi::logger",
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config:
+      env("NODE_ENV") === "production"
+        ? {
+            contentSecurityPolicy: {
+              useDefaults: true,
+              directives: {
+                "connect-src": ["'self'", "https:"],
+                "img-src": [
+                  "'self'",
+                  "data:",
+                  "blob:",
+                  "market-assets.strapi.io",
+                  env("CF_PUBLIC_ACCESS_URL")
+                    ? env("CF_PUBLIC_ACCESS_URL").replace(/^https?:\/\//, "")
+                    : "",
+                ],
+                "media-src": [
+                  "'self'",
+                  "data:",
+                  "blob:",
+                  "market-assets.strapi.io",
+                  env("CF_PUBLIC_ACCESS_URL")
+                    ? env("CF_PUBLIC_ACCESS_URL").replace(/^https?:\/\//, "")
+                    : "",
+                ],
+                upgradeInsecureRequests: null,
+              },
+            },
+          }
+        : {},
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
